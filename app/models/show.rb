a67d810e4256030ac938100e7384a4fa5644a9c2
remote-torrent-6747 <abbyc@sky.com>
2021-03-27 19:19:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    return highest_rating.title
+    return @highest_rating.title
   end
   return the name of the show with the highest rating
 
